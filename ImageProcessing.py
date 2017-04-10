9e10fd2ecd58431e4cb76b7dbd1bcba84a49153a
matthewruhland <matt.ruhland18@gmail.com>
2017-04-10 22:51:02 +0000

    added changes to messages

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -15,7 +15,6 @@ GPIO.setwarnings(False)
 GPIO.setup(8,GPIO.OUT)
 
 
-
 def LaserOn():
     GPIO.output(8,GPIO.HIGH)
 
@@ -32,7 +31,6 @@ def ScanImage(filename):
     width,height = im_grey_shrunk.size
     print "Pixels ",width,height," "
 
-    #value=[[0 for x in range(0,width+1)] for y in range(0,height+1)]
     value = []
     for i in range(width):
         value.append([])
@@ -59,7 +57,6 @@ def MoveLaserMotor(direction, numberOfHalfSteps):
     GPIO.setup(23,GPIO.OUT) #step pin
     GPIO.setup(7, GPIO.IN)
     
-
     if(direction == True):
         GPIO.output(18,GPIO.HIGH) #set direction clockwise
     elif(direction == False):#not sure if this is correct for backwards
@@ -94,11 +91,10 @@ def StationMotor(direction, numberOfHalfSteps):
         time.sleep(0.001)
         if (GPIO.input(7) == True): StopEverything()
 
-
     GPIO.cleanup()
 
 def set_to_RefPoint(PixelSize):
-    for i in range(30):
+    for i in range(50):
         StationMotor(True, PixelSize)
         time.sleep(0.01)
         MoveLaserMotor(False, PixelSize)
@@ -154,7 +150,6 @@ def D2A(D2AVal):            #0 = highest voltage (~4.8V, theoretically)
         D2AVal = D2AVal << 1
         GPIO.output(19,GPIO.HIGH)
 
-
     #CS goes high to terminate communication
     GPIO.output(13,GPIO.HIGH)
 
@@ -165,56 +160,54 @@ def EngravePixels(TwodArrayofPixels, OnePixelSize):
     width = len(TwodArrayofPixels)          #columns?
     height = len(TwodArrayofPixels[0])      #rows?
     print width,height
-    for i in range(width - 1):
+    for i in range(width):
         if(i%2 == 0):                                     
             for j in range(height):
 ##                pixelBurn(TwodArrayofPixels[i][j])
-                time.sleep(0.1)
+                time.sleep(0.5)
                 if (j == height-1): break
                 MoveLaserMotor(False, OnePixelSize)
         else:
             for j in range(height - 1,-1,-1):
 ##                pixelBurn(TwodArrayofPixels[i][j])
-                time.sleep(0.1)
+                time.sleep(0.5)
                 if (j == 0): break   #if at end of row,  
                 MoveLaserMotor(True, OnePixelSize)
-                
+
+        if (i == width - 1): break
         StationMotor(True, OnePixelSize) 
         print "New Row"
 
-##def FinishBurning():
-##        result = tkMessageBox.askquestion("done?", "Laser will stop", icon = 'warning')
-##    if result == 'yes'
-##        print "done"
-##    else
-##        print "not done"tkMessageBox
+def reset(TwoDarray, PixelSize):
+    width_length = len(TwoDarray)
+    if(width_length%2 == 0):
+        for i in range(width_length):
+        	StationMotor(False, PixelSize)
+        	time.sleep(0.1)
+    else:
+        for i in range(width_length):
+        	StationMotor(False, PixelSize)
+        	time.sleep(0.1)
+       		MoveLaserMotor(True, PixelSize)
+        	time.sleep(0.1)
 
 ################### Where the Magic Happens ####################
 #This now takes pictures every row and promps user for a file to use
-#count Variables 
 
 def main():
     result = True
     root = Tkinter.Tk()
-    root.withdraw()
     while(result):
-    	result = False
-        file = tkFileDialog.askopenfile( mode ='rb',title='Please select a file')
-        
+        file = tkFileDialog.askopenfile(parent=root, mode ='rb',title='Please select a file') 
         pixels2DArray = ScanImage(file)
         EngravePixels(pixels2DArray, PixelTestingSize)
-        result = tkMessageBox.askyesno("Laser", "Continue Burning?")
+        result = tkMessageBox.askyesno("Laser", "Engraving complete. Click Yes if you want to burn another image. Click No to quit.")
         if result:
-            result = tkMessageBox.askyesno("Laser", "Is new burning material in place")
+            result = tkMessageBox.askyesno("Laser", "Turn laser off and load new material now. Click Yes when ready or No to quit.")
         print result
-    ##ask to continue window, reset
-    #B1 = Tkinter.Button(root, text = "Delete", command = FinishBurning)
-    #B1.pack()
-    #top.mainloop()
+        reset(pixels2DArray, PixelTestingSize)
 
     
-    ##reset()
-    
-PixelTestingSize = 30 #steps of Motor per Pixel
+PixelTestingSize = 25 #steps of Motor per Pixel
 set_to_RefPoint(PixelTestingSize)
 main()
